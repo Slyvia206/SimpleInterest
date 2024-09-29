fa755c041ac4a712a4b96dab4e91f59df5a2a702
Slyvia206 <muriithislyvia@gmail.com>
2024-09-29 06:37:28 -0700

    Add files via upload

---
+++ SIMPLE INTEREST.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (){
+	float Pr, T, R, SI
+	
+	printf("Enter Pr: ");
+	scanf("%f", &Pr);
+	
+	printf("Enter T: ");
+	scanf("%f", &T);
+	
+
+	printf("Enter R: ");
+	scanf("%f", &R);
+	
+
+	SI = ((Pr*R*T)/100);
+	
+printf("Simple Intrest: %f" ,SI);
+
+return 0;
+}
